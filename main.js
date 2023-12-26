15eae03658cbb191ac42723a1d57fbc93672126e
nakama ayano <chucaaan@gmail.com>
2023-12-27 02:14:27 +0900

    js test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-fetch("header.html")
+fetch("include/header.html")
   .then((response) => response.text())
   .then((data) => document.querySelector("#header").innerHTML = data);
-fetch("footer.html")
+fetch("include/footer.html")
   .then((response) => response.text())
   .then((data) => document.querySelector("#footer").innerHTML = data);
